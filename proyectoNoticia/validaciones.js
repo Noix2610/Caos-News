ea3aa7debdf03d82eaacd0cc5bbac87ae7a58f01
Jose Pino Araya <j.pino2610@gmail.com>
2024-04-21 21:24:38 -0400

    Inicio Sesion y Registro 2.0

--- proyectoNoticia/validaciones.js
+++ proyectoNoticia/validaciones.js
@@ -20,41 +20,46 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function validarUsuario(input, div) {
         var texto = input.value;
-
-        if (texto.length < 4 || texto.length > 15) {
-            div.style.color = "red";
-            div.innerText = "El texto debe contener entre 4 y 15 caracteres.";
-        } else {
-            div.style.color = "green";
-            div.innerText = "Válido";
+        if (texto.length > 0) {
+            if (texto.length < 4 || texto.length > 15) {
+                div.style.color = "red";
+                div.innerText = "El texto debe contener entre 4 y 15 caracteres.";
+            } else {
+                div.style.color = "green";
+                div.innerText = "Válido";
+            }
+        }else{
+            div.innerText = "";
         }
     }
-
-
 
     function validarCampo(input, div) {
         var texto = input.value;
         var contieneNumeros = false;
-
-        for (var i = 0; i < texto.length; i++) {
-            if (!isNaN(texto.charAt(i))) {
-                contieneNumeros = true;
-                break;
+        if (texto.length > 0) {
+            for (var i = 0; i < texto.length; i++) {
+                if (!isNaN(texto.charAt(i))) {
+                    contieneNumeros = true;
+                    break;
+                }
             }
-        }
 
-        if (contieneNumeros) {
-            div.style.color = "red";
-            div.innerText = "El texto no puede contener números.";
-            return;
-        } else if (texto.length < 3 || texto.length > 20) {
-            div.style.color = "red";
-            div.innerText = "El tamaño del texto debe tener entre 3 y 20 caracteres."
-        } else {
-            div.style.color = "green";
-            div.innerText = "Válido";
-        }
 
+            if (contieneNumeros) {
+                div.style.color = "red";
+                div.innerText = "El texto no puede contener números.";
+                return;
+            } else if (texto.length < 3 || texto.length > 20) {
+                div.style.color = "red";
+                div.innerText = "El tamaño del texto debe tener entre 3 y 20 caracteres."
+            } else {
+                div.style.color = "green";
+                div.innerText = "Válido";
+            }
+
+        }else {
+            div.innerText = "";
+        }
     }
 });
 
