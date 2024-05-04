5d5db38124a1d2dc5177a0d7070c359f3b8def06
Jose Pino <j.pino2610@gmail.com>
2024-05-04 17:23:41 -0400

    Actualizacion de detalles
    Se arregla codigo jquery agregado, se ajustan algunos detalles en pagina principal y deportes

---
+++ proyectoNoticia/jquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $("#btn1").mouseenter(function () {
+        $("#tlt1").hide(500);
+    });
+    $("#btn1").mouseleave(function () {
+        $("#tlt1").show(500);
+    });
+
+    $("#btn2").mouseenter(function () {
+        $("#tlt2").hide(500);
+    });
+    $("#btn2").mouseleave(function () {
+        $("#tlt2").show(500);
+    });
+
+    $("#btn3").mouseenter(function () {
+        $("#tlt3").hide(1000);
+    });
+    $("#btn3").mouseleave(function () {
+        $("#tlt3").show(1000);
+    });
+
+    $("#btn4").mouseenter(function () {
+        $("#tlt4").hide(1000);
+    });
+    $("#btn4").mouseleave(function () {
+        $("#tlt4").show(1000);
+    });
+
+});
